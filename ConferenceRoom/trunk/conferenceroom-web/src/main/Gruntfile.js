f8ed773acdda8dc91e7c884b29084e1eb5614dc8
wei.ru <wei.ru@7f197fd0-fd35-4ced-8fd1-e6322d62bb6f>
2015-02-24 05:08:35 +0000

    remove not needed tasks

--- ConferenceRoom/trunk/conferenceroom-web/src/main/Gruntfile.js
+++ ConferenceRoom/trunk/conferenceroom-web/src/main/Gruntfile.js
@@ -17,7 +17,7 @@ module.exports = function (grunt) {
 
   // Configurable paths for the application
   var appConfig = {
-    app: require('./bower.json').appPath || 'app',
+    app: require('./bower.json').appPath || 'webapp',
     dist: 'dist'
   };
 
@@ -387,27 +387,6 @@ module.exports = function (grunt) {
         singleRun: true
       }
     }
-  });
-
-
-  grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
-    if (target === 'dist') {
-      return grunt.task.run(['build', 'connect:dist:keepalive']);
-    }
-
-    grunt.task.run([
-      'clean:server',
-      'wiredep',
-      'concurrent:server',
-      'autoprefixer:server',
-      'connect:livereload',
-      'watch'
-    ]);
-  });
-
-  grunt.registerTask('server', 'DEPRECATED TASK. Use the "serve" task instead', function (target) {
-    grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
-    grunt.task.run(['serve:' + target]);
   });
 
   grunt.registerTask('test', [
