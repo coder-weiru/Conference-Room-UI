fbb5d28f41c46fc596b8da60791f0691b6cbc106
wei.ru <wei.ru@7f197fd0-fd35-4ced-8fd1-e6322d62bb6f>
2015-03-04 05:14:44 +0000

    useradmin page using ui-grid

--- webapp/scripts/controllers/useradmin_controller.js
+++ webapp/scripts/controllers/useradmin_controller.js
@@ -1,18 +1,47 @@
 /**
  * UserAdminController
  */
-var userModule = angular.module('controller.userAdmin', [ 'service.userAdmin' ]);
+var userModule = angular.module('controller.userAdmin', [ 'service.userAdmin', 'ui.grid' ]);
 
 userModule.controller('UserListCtrl', function($scope, UserAdminService) {
-	
+	$scope.columns = [{ name: ' ',
+                        cellTemplate: '<img ng-src="images/headshot-empty.jpg"/>',
+                        cellClass: 'ngCellText',
+                        width: 100,
+                        enableHiding: false,
+                        enableSorting: false,
+                        enableColumnMenu: false
+                      }, 
+                      { field: 'name',
+                        cellClass: 'ngCellText',
+                      }, 
+                      { field: 'email', 
+                        cellClass: 'ngCellText',
+                      }, 
+                      { field: 'group',
+                        cellClass: 'ngCellText',
+                      }];
+    
+    $scope.gridOptions = {
+        rowHeight:130,
+        enableRowSelection: true, 
+        enableRowHeaderSelection: false,
+        enableSorting: true,
+        columnDefs: $scope.columns,
+        onRegisterApi: function(gridApi) {
+          $scope.gridApi = gridApi;
+        }
+    };
 	$scope.users = [];
     
 	$scope.listUsers = function() {
-		UserAdminService.lisUsers().then(function(users) {
+		UserAdminService.listUsers().then(function(users) {
 			$scope.users = users;
+            $scope.gridOptions.data = $scope.users;
 		});
 	};
 	
+    
 	$scope.listUsers();
 	
 });
