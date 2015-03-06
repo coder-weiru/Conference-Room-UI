032866e20e83e9fa37c4f8c12642400687693a83
wei.ru <wei.ru@7f197fd0-fd35-4ced-8fd1-e6322d62bb6f>
2015-03-06 21:39:11 +0000

    Add message_box dialog to show update error

--- ConferenceRoom/trunk/conferenceroom-web/src/main/webapp/scripts/controllers/useradmin_controller.js
+++ ConferenceRoom/trunk/conferenceroom-web/src/main/webapp/scripts/controllers/useradmin_controller.js
@@ -1,9 +1,9 @@
 /**
  * UserAdminController
  */
-var userModule = angular.module('controller.userAdmin', [ 'service.userAdmin',  'service.userPhoto', 'ui.grid' ]);
+var userModule = angular.module('controller.userAdmin', [ 'service.userAdmin',  'service.userPhoto', 'service.messageBox', 'ui.grid', 'ui.grid.edit', 'ui.grid.rowEdit', 'ui.grid.cellNav']);
 
-userModule.controller('UserListCtrl', function($scope, UserAdminService, UserPhotoModalService) {
+userModule.controller('UserListCtrl', function($scope, $log, UserAdminService, UserPhotoModalService, MessageBoxService) {
     
     $scope.photo = function(val) {
         var photoUrl = "images/headshot-empty.jpg";
@@ -19,16 +19,20 @@ userModule.controller('UserListCtrl', function($scope, UserAdminService, UserPho
                         width: 100,
                         enableHiding: false,
                         enableSorting: false,
-                        enableColumnMenu: false
+                        enableColumnMenu: false,
+                        enableCellEdit: false
                       }, 
                       { field: 'name',
                         cellClass: 'ngCellText',
+                        enableCellEdit: true
                       }, 
                       { field: 'email', 
                         cellClass: 'ngCellText',
+                        enableCellEdit: true
                       }, 
                       { field: 'group',
                         cellClass: 'ngCellText',
+                        enableCellEdit: true
                       }];
     
     $scope.gridOptions = {
@@ -36,12 +40,69 @@ userModule.controller('UserListCtrl', function($scope, UserAdminService, UserPho
         enableRowSelection: true, 
         enableRowHeaderSelection: false,
         enableSorting: true,
+        enableCellEditOnFocus: true,
         columnDefs: $scope.columns,
         onRegisterApi: function(gridApi) {
           $scope.gridApi = gridApi;
+          gridApi.rowEdit.on.saveRow($scope, $scope.saveRow);
         }
     };
-	$scope.users = [];
+ 
+    var fileUploadModalOptions = {
+        closeButtonText: 'Cancel',
+        actionButtonText: 'Upload picture file...',
+        headerText: 'Upload Headshot Photo',
+        bodyText: 'Please select a headshot picture file to upload.'
+    };
+
+    $scope.openPhotoUploadModalDialog = function( rowEntity ) {
+		UserPhotoModalService.showModal({}, fileUploadModalOptions).then(function (result) {
+            console.log(result);
+        });
+	};
+    
+    $scope.saveRow = function( rowEntity ) {
+        var promise = UserAdminService.updateUser( rowEntity );
+        $scope.gridApi.rowEdit.setSavePromise( rowEntity, promise );
+        
+        promise.then(function(response) {
+			var data = response.data;                   
+			if (data.httpStatus == 'OK') {
+			    $scope.errorMessage = 'User information is saved successfully!';
+		    } else {
+		    	$scope.errorMessage = 'Oops, we received your request, but there was an error processing it.';
+                $scope.openErrorMessageModalDialog('Error while updating user', $scope.errorMessage);
+		    }
+            $log.error(data);
+		}, function(response) {
+            var data = response.data;                   
+			if (data.httpStatus == 'BAD_REQUEST') {
+			    $scope.errorMessage = response.data.message;
+                $scope.openErrorMessageModalDialog('Error while updating user', $scope.errorMessage);
+		    } else {
+		    	$scope.errorMessage = 'There was a network error. Try again later.';
+                $scope.openErrorMessageModalDialog('Error while updating user', $scope.errorMessage);
+		    }
+		    $log.error(data);
+		});
+        
+    }; 
+ 
+    var messageBoxModalOptions = {
+            actionButtonText: '  Got It ',
+            headerText: '',
+            bodyText: ''
+    };
+    
+    $scope.openErrorMessageModalDialog = function( header, message ) {
+        messageBoxModalOptions.headerText = header;
+        messageBoxModalOptions.bodyText = message;                                                              
+        MessageBoxService.showModal({}, messageBoxModalOptions).then(function (result) {
+            console.log(result);
+        });
+	}; 
+    
+    $scope.users = [];
     
 	$scope.listUsers = function() {
 		UserAdminService.listUsers().then(function(users) {
@@ -50,19 +111,6 @@ userModule.controller('UserListCtrl', function($scope, UserAdminService, UserPho
 		});
 	};
 	
-    var modalOptions = {
-        closeButtonText: 'Cancel',
-        actionButtonText: 'Upload picture file...',
-        headerText: 'Upload Headshot Photo',
-        bodyText: 'Please select a headshot picture file to upload.'
-    };
-
-    $scope.openPhotoUploadModalDialog = function(user) {
-		UserPhotoModalService.showModal({}, modalOptions).then(function (result) {
-            console.log(result);
-        });
-	};
-    
 	$scope.listUsers();
 	
 });
