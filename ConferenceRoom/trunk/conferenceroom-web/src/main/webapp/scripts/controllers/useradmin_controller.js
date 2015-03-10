eb593fcdaefd9bfab55d800d491ceb2ffb8269b7
wei.ru <wei.ru@7f197fd0-fd35-4ced-8fd1-e6322d62bb6f>
2015-03-10 05:01:16 +0000

    Add new user and selection

--- ConferenceRoom/trunk/conferenceroom-web/src/main/webapp/scripts/controllers/useradmin_controller.js
+++ ConferenceRoom/trunk/conferenceroom-web/src/main/webapp/scripts/controllers/useradmin_controller.js
@@ -3,7 +3,7 @@
  */
 var userModule = angular.module('controller.userAdmin', [ 'service.userAdmin',  'service.userPhoto', 'service.messageBox', 'ui.grid', 'ui.grid.selection', 'ui.grid.edit', 'ui.grid.rowEdit', 'ui.grid.cellNav']);
 
-userModule.controller('UserListCtrl', function($scope, $log, UserAdminService, UserPhotoModalService, MessageBoxService) {
+userModule.controller('UserListCtrl', function($scope, $log, $timeout, UserAdminService, UserPhotoModalService, MessageBoxService) {
     
     $scope.getPhotoUrl = function( rowEntity ) {
         var photoUrl = rowEntity.photoUrl;
@@ -71,29 +71,31 @@ userModule.controller('UserListCtrl', function($scope, $log, UserAdminService, U
         });
 	  };
     
-    $scope.saveRow = function( rowEntity ) {
-        var promise = UserAdminService.updateUser( rowEntity );
+    $scope.saveRow = function( rowEntity ) { debugger;
+        if (rowEntity!=null && rowEntity.id==null) {
+			promise = UserAdminService.addUser( rowEntity );
+		} else {
+			promise = UserAdminService.updateUser( rowEntity );
+		}
         $scope.gridApi.rowEdit.setSavePromise( rowEntity, promise );
         
-        promise.then(function(response) {
+        promise.then(function(response) { debugger;
 			var data = response.data;                   
-			if (data.httpStatus == 'OK') {
+			if (response.statusText == 'OK') {
 			    $scope.errorMessage = 'User information is saved successfully!';
 		    } else {
 		    	$scope.errorMessage = 'Oops, we received your request, but there was an error processing it.';
                 $scope.openErrorMessageModalDialog('Error while updating user', $scope.errorMessage);
 		    }
-            $log.error(data);
-		}, function(response) {
+		}, function(response) { debugger;
             var data = response.data;                   
-			if (data.httpStatus == 'BAD_REQUEST') {
+			if (data.httpStatus == 'BAD_REQUEST' || data.httpStatus == 'INTERNAL_SERVER_ERROR') {
 			    $scope.errorMessage = response.data.message;
                 $scope.openErrorMessageModalDialog('Error while updating user', $scope.errorMessage);
 		    } else {
 		    	$scope.errorMessage = 'There was a network error. Try again later.';
                 $scope.openErrorMessageModalDialog('Error while updating user', $scope.errorMessage);
 		    }
-		    $log.error(data);
 		});
         
     }; 
@@ -112,31 +114,44 @@ userModule.controller('UserListCtrl', function($scope, $log, UserAdminService, U
         });
 	}; 
     
-  $scope.users = [];
-  $scope.selectedUser;
-
-	$scope.listUsers = function() {
+    $scope.listUsers = function() {
 		UserAdminService.listUsers().then(function(users) {
-			$scope.users = users;
-            $scope.gridOptions.data = $scope.users;
+			$scope.gridOptions.data = users;
 		});
 	};
 	
-	$scope.listUsers();
+    $scope.addUser = function( user ) {
+		$scope.gridOptions.data.unshift( user );
+        $scope.gridApi.rowEdit.flushDirtyRows( $scope.gridApi.grid );
+        $scope.gridApi.selection.selectRow($scope.gridOptions.data[0]);
+	};
+	
+    $scope.selectedUser;
+    $scope.listUsers();
 	
 });
 
 userModule.controller('MenuCtrl', function ($scope, $log) {
-  $scope.canAddNewUser = function() {
-    //return $scope.selectedUser!=null;
-    return true;
-  };
+    $scope.canAddNewUser = function() {
+        //return $scope.selectedUser!=null;
+        return true;
+    };
 
-  $scope.canUploadPhoto = function() {
-    return $scope.selectedUser!=null;
-  };
+    $scope.canUploadPhoto = function() {
+        return $scope.selectedUser!=null;
+    };
 
-  $scope.canDeleteUser = function() {
-    return $scope.selectedUser!=null;
-  };
+    $scope.canDeleteUser = function() {
+        return $scope.selectedUser!=null;
+    };
+    
+    $scope.addNewUser = function() {
+		var user = {};
+		user.name = 'New User';
+		user.email = 'New User Email';
+		user.group = false;
+        user.photoUrl = ' ';
+                                    
+		$scope.addUser(user);
+	};
 });
