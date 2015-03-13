05157c5d11913cfc30c391204a5eb60c7fa6ce24
wei.ru <wei.ru@7f197fd0-fd35-4ced-8fd1-e6322d62bb6f>
2015-03-13 23:12:11 +0000

    Carousel $watch event to display Room details

--- webapp/scripts/controllers/roomadmin_controller.js
+++ webapp/scripts/controllers/roomadmin_controller.js
@@ -4,9 +4,10 @@
  */
 var roomModule = angular.module('controller.roomAdmin', [ 'service.roomAdmin', 'ui.bootstrap']);
 
-roomModule.controller('RoomCarouselCtrl', function($scope, $log, $timeout, $modal, RoomAdminService) {
+roomModule.controller('RoomCarouselCtrl', function($scope, $rootScope, $log, $timeout, $modal, RoomAdminService) {
     
     $scope.slideInterval = 5000;
+    $scope.rooms = [];
     var slides = $scope.slides = [];
     
     $scope.clearSlides = function() {
@@ -25,6 +26,7 @@ roomModule.controller('RoomCarouselCtrl', function($scope, $log, $timeout, $moda
     
     $scope.listRooms = function() { 
 		RoomAdminService.listRooms().then(function(rooms) {
+            $scope.rooms = rooms;
 			$scope.clearSlides();
             for ( var i = 0; i < rooms.length; i++ ) {
                 $scope.addSlide( rooms[i] );
@@ -33,4 +35,91 @@ roomModule.controller('RoomCarouselCtrl', function($scope, $log, $timeout, $moda
 	};
     
     $scope.listRooms();
+    
+    $scope.$watch(function () {
+      for (var i = 0; i < slides.length; i++) {
+        if (slides[i].active) {
+          return slides[i];
+        }
+      }
+    }, function (currentSlide, previousSlide) {
+      if (currentSlide !== previousSlide) {
+          console.log('currentSlide:', currentSlide);
+          $rootScope.$broadcast('slideChange', slides.indexOf(currentSlide));
+      }
+    });
+});
+
+roomModule.controller('RoomCtrl', function($scope, $log, RoomAdminService) {
+	
+    var room = $scope.room = {};
+    
+	$scope.saveRoom = function () {
+		// If form is invalid, return and let AngularJS show validation errors.
+		if ($scope.roomForm.$invalid) {
+		    return;
+		}
+		var promise;
+		if ($scope.isNewRoom($scope.room)) {
+			promise = RoomAdminService.addRoom($scope.room);
+		} else {
+			promise = RoomAdminService.updateRoom($scope.room);
+		}
+        promise.then(function(response) { 
+			var data = response.data;                   
+			if (response.statusText == 'OK') {
+			    $scope.errorMessage = 'Room information is saved successfully!';
+                $scope.$broadcast('roomSaved', data);
+		    } else {
+		    	$scope.showMessage('Oops, we received your request, but there was an error processing it.');
+                $scope.$broadcast('roomSaveErr', user);
+		    }
+		}, function(response) { 
+            var data = response.data;                   
+			if (data.httpStatus == 'BAD_REQUEST' || data.httpStatus == 'INTERNAL_SERVER_ERROR') {
+			    $scope.showMessage(response.data.message);
+		    } else {
+		    	$scope.showMessage('There was a network error. Try again later.');
+		    }
+            $scope.$broadcast('roomSaveErr', user);
+		});
+	};
+    
+	$scope.revertRoom = function() {
+		$scope.room = angular.copy($scope.originalUser(), $scope.room);
+		$scope.roomForm.$setPristine();
+	};
+	
+	$scope.canRevert = function() {
+		return !angular.equals($scope.room, $scope.originalRoom());
+	};
+	
+	$scope.canSave = function() {
+	    return $scope.roomForm.$valid && !angular.equals($scope.room, $scope.originalRoom());
+	};
+	
+	$scope.cancel = function() {
+		if ($scope.isNewRoom($scope.room)) {
+			$scope.deleteNewRoom();
+		} else {
+			$scope.revertRoom();
+		}
+		$scope.deSelectRoom();
+	};
+	
+	$scope.originalRoom = function() {
+		var origRoom = null;
+		angular.forEach($scope.originalRoomList, function( room ) {
+			var uid = $scope.selectedRoom.id;
+			if (uid===room.id) {
+		    	origRoom = room;
+		    } 
+		});
+		return origRoom;
+	};	
+    
+    $scope.$on('slideChange', function(event, index) { 
+        $scope.room = $scope.rooms[index];
+    });
+	
 });
