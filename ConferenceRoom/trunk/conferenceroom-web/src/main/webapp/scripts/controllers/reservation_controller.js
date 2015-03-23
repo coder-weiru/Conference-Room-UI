90e9fa09eb6b0078d2cc9cbef9f217a1595951dc
wei.ru <wei.ru@7f197fd0-fd35-4ced-8fd1-e6322d62bb6f>
2015-03-23 04:53:40 +0000

    Add new reservation

--- ConferenceRoom/trunk/conferenceroom-web/src/main/webapp/scripts/controllers/reservation_controller.js
+++ ConferenceRoom/trunk/conferenceroom-web/src/main/webapp/scripts/controllers/reservation_controller.js
@@ -2,62 +2,80 @@
 /**
  * ReservationController
  */
-var reservationModule = angular.module('controller.reservation', [ 'service.reservation', 'service.roomAdmin', 'service.messageBox', 'ui.calendar', 'ui.bootstrap' ]);
+var reservationModule = angular.module('controller.reservation', [ 'service.reservation', 'service.roomAdmin', 'service.userAdmin', 'service.messageBox', 'angularSpinner', 'ui.calendar', 'ui.bootstrap', 'ui.bootstrap.datetimepicker' ]);
 
-reservationModule.controller('CalendarCtrl', function($scope, $rootScope, $log, $timeout, $modal, $msgbox, RoomAdminService, ReservationService) {
+reservationModule.controller('CalendarCtrl', function($scope, $rootScope, $log, $timeout, $modal, $msgbox, ReservationService) {
      
+    function dateFromISO8601(isostr) { debugger;
+        var localTimeS = isostr.slice(0, isostr.indexOf('Z'));    
+        var localTime = moment(isostr).subtract(moment().utcOffset(), 'm');
+        var datetime = localTime.toDate();     
+        var d = datetime.getDate();
+        var m = datetime.getMonth();
+        var y = datetime.getFullYear();
+        var h = datetime.getHours();
+        var min = datetime.getMinutes();  
+        var s = datetime.getSeconds();
+        var date = new Date($scope.currentDate);
+          date.setTime(Date.parse(localTimeS));                              
+        return date;                                
+    }
+    
     $scope.events = [];
-     
+    $scope.eventStart = moment();
+    $scope.eventEnd = moment();
+    
     $scope.getEvents = function(start, end) { 
         ReservationService.listReservations(start, end).then(function(reservations) { 
             var events = [];
-            reservations.forEach(function (element) {
+            reservations.forEach(function (element) { debugger;
                   events.push(
                     {
                          title: element.title,
                          description: element.description,
-                         start: new Date(element.startTime), 
-                         end: new Date(element.endTime), 
+                         start: dateFromISO8601(element.startTime), 
+                         end: dateFromISO8601(element.endTime), 
                          creator: element.creator,
-                         room: element.room.name,
+                         room: element.room,
                          editable: true
                     });
             });
+            
             $scope.events = events;
 		});
     };
     
-    $scope.onDayClick = function( date, allDay, jsEvent, view ){ 
+    $scope.onDayClick = function( date, allDay, jsEvent, view ) { 
         $scope.$apply(function(){
           $log.info('Day Clicked ' + date);
         });
     };
      
      
-    $scope.onEventDrop = function(event, dayDelta, minuteDelta, allDay, revertFunc, jsEvent, ui, view){ 
+    $scope.onEventDrop = function(event, dayDelta, minuteDelta, allDay, revertFunc, jsEvent, ui, view) { 
         $scope.$apply(function(){
           $log.info('Event Droped to make dayDelta ' + dayDelta);
         });
     };
      
      
-    $scope.onEventResize = function(event, dayDelta, minuteDelta, revertFunc, jsEvent, ui, view ){  
+    $scope.onEventResize = function(event, dayDelta, minuteDelta, revertFunc, jsEvent, ui, view ) {  
         $scope.$apply(function(){
           $log.info('Event Resized to make dayDelta ' + minuteDelta);
         });
     };
      
-    $scope.onEventClick = function(event){           
+    $scope.onEventClick = function(event) {           
         $scope.$apply(function(){
           $log.info(event.title + ' is clicked');
         });
     };
      
     $scope.onRenderView = function(view){    
-        var date = new Date(view.calendar.getDate());
-        $scope.currentDate = date.toDateString();
+        $scope.currentDate = view.calendar.getDate()
         $scope.$apply(function(){
-            $log.info('Page render with date '+ $scope.currentDate);
+            var date = new Date($scope.currentDate);
+            $log.info('Page render with date '+ date.toDateString());
             var start = moment(date).set({'hour': 0, 'minute': 0, 'second': 0, 'millisecond': 0});
           
             var end = moment(date).set({'hour': 23, 'minute': 59, 'second': 59, 'millisecond': 999});
@@ -65,6 +83,14 @@ reservationModule.controller('CalendarCtrl', function($scope, $rootScope, $log, 
         });
     };
      
+    $scope.onSelect = function( start, end, jsEvent, view ) {
+        $scope.eventStart = start;
+        $scope.eventEnd = end;
+        $scope.$apply(function(){
+            $log.info('Time period selected from '+ start.format() + ' to ' + end.format());
+        });
+    }
+    
     $scope.changeView = function(view,calendar) {
         currentView = view;
         calendar.fullCalendar('changeView',view);
@@ -94,16 +120,43 @@ reservationModule.controller('CalendarCtrl', function($scope, $rootScope, $log, 
         eventDrop: $scope.onEventDrop,
         eventResize: $scope.onEventResize,
         eventClick: $scope.onEventClick,
-        viewRender: $scope.onRenderView
+        viewRender: $scope.onRenderView,
+        select: $scope.onSelect
     };
+    
+    $scope.$watch(function(scope) { 
+            return scope.eventStart;
+        },
+        function(currStart, prevStart) {       
+            $rootScope.$broadcast('eventStartChanged', currStart);
+        }
+    );
+    
+    $scope.$watch(function(scope) { 
+            return scope.eventEnd;
+        },
+        function(currEnd, prevEnd) {        
+            $rootScope.$broadcast('eventEndChanged', currEnd);
+        }
+    );
+    
+    $scope.$watch(function(scope) { 
+            return scope.currentDate;
+        },
+        function(currentDate, prevCurrentDate) {        
+            $rootScope.$broadcast('currentDateChanged', currentDate);
+        }
+    );
     
     $scope.eventSources = [$scope.events];
 });
 
-reservationModule.controller('RoomListCtrl', function($scope, $rootScope, $log, $timeout, $modal, $msgbox, RoomAdminService) {
+reservationModule.controller('RoomListCtrl', function($scope, $rootScope, $log, $timeout, $modal, RoomAdminService) {
     
     var roomGrid = $scope.roomGrid = [];
     var rooms = $scope.rooms = [];
+    $scope.eventStartDate = new Date();
+    $scope.eventEndDate = new Date();
     
     $scope.listRooms = function() { 
 		RoomAdminService.listRooms().then(function(rooms) { 
@@ -131,6 +184,170 @@ reservationModule.controller('RoomListCtrl', function($scope, $rootScope, $log, 
         }            
     };
     
+    $scope.addEvent = function( room ) { 
+        var modalInstance = $modal.open({
+          templateUrl: 'views/partials/new_event.html',
+          controller: 'NewEventCtrl',
+          //size: 'lg',
+          resolve: {
+            room: function () {
+                return room;
+            },
+            currentDate: function () {
+                return $scope.currentDate;
+            },
+            start: function () {
+                return $scope.eventStartDate;
+            },
+            end: function () {
+                return $scope.eventEndDate;
+            }
+          }
+        });
+
+        modalInstance.result.then(function (message) {
+          if (message=='ok') {
+                
+              }
+            }, 
+            function () {
+              $log.info('Modal dismissed at: ' + new Date());
+          }
+        );
+    };
+    
     $scope.listRooms();
     
+    $scope.$on('eventStartChanged', function(event, eventStart) {  
+        
+        var localTime = moment(eventStart).subtract(moment().utcOffset(), 'm');
+                                                                 
+        $scope.eventStartDate = localTime.toDate();
+            
+    });
+    
+    $scope.$on('eventEndChanged', function(event, eventEnd) {   
+           
+        var localTime = moment(eventEnd).subtract(moment().utcOffset(), 'm');
+                                                                 
+        $scope.eventEndDate = localTime.toDate();
+            
+    });
+    
+    $scope.$on('currentDateChanged', function(event, currentDate) {   
+           
+        $scope.currentDate = currentDate;
+            
+    });
+});
+
+reservationModule.controller('NewEventCtrl', function ($scope, $modalInstance, $msgbox, usSpinnerService, UserAdminService, ReservationService, room, currentDate, start, end) { 
+    
+    $scope.minDate = new Date();
+    $scope.datetimePickerOptions = {
+        dateOptions: {
+            startingDay: 1,
+            showWeeks: false
+        },
+        minuteStep: 15,
+        showMeridian: true,
+        dateFormat: "dd-MMM-yyyy",
+        readonlyTime: false,
+        minDate: $scope.minDate,
+        dateDisabled: function(calendarDate, mode) {
+            return ($scope.currentDate != null) && (mode === 'day') && ( $scope.currentDate.day() === 0 || $scope.currentDate.day() === 6 );
+        }
+    };
+    
+    $scope.room = room;
+    $scope.currentDate = currentDate;
+    $scope.editTime = false;
+    $scope.editDetails = false;
+    $scope.users = [];
+    $scope.reservation = {};
+    $scope.reservation.startTime = start;
+    $scope.reservation.endTime = end;
+    
+    $scope.toggleEditDetails = function() { 
+        $scope.editDetails = !$scope.editDetails;
+    };
+    
+    $scope.toggleEditTime = function() { 
+        $scope.editTime = !$scope.editTime;
+    };
+    
+    $scope.listUsers = function() {
+		UserAdminService.listUsers().then(function(users) {
+			$scope.users = users;
+		});
+	};
+	
+    $scope.saveReservation = function () { debugger;
+        
+        //$scope.startSpin();
+        
+		// If form is invalid, return and let AngularJS show validation errors.
+		if ($scope.eventForm.$invalid) {
+		    return;
+		}
+		var promise;
+		if ($scope.isNewReservation($scope.reservation)) {
+			promise = ReservationService.makeReservation($scope.room.id, $scope.reservation);
+		} else {
+			promise = ReservationService.updateReservation($scope.room.id, $scope.reservation);
+		}
+        promise.then(function(response) {  debugger;
+            
+            //$scope.stopSpin();
+            
+			var data = response.data;                   
+			if (response.statusText == 'OK') {
+			    $scope.$broadcast('reservationSaved', data);
+		    } else {
+		    	$msgbox.showErrorMessage('Oops, we received your request for saving ' + $scope.reservation.title + ' , but there was an error processing it.');
+                $scope.$broadcast('reservationSaveErr', data);
+		    }
+		}, function(response) { 
+            var data = response.data;                   
+			if (data.httpStatus == 'BAD_REQUEST' || data.httpStatus == 'INTERNAL_SERVER_ERROR') {
+			    $msgbox.showErrorMessage(response.data.message);
+		    } else {
+		    	$msgbox.showErrorMessage('There was a network error while saving  ' + $scope.reservation.title + '. Try again later.');
+		    }
+            $scope.$broadcast('reservationSaveErr', data);
+		});
+	};
+    
+    $scope.filterUser = function( searchText ) { 
+        if (searchText!=null && searchText.trim()!='') {
+			var filtered = $scope.users.filter(function( user ){
+                return !user.name.indexOf(searchText);
+            });
+            return filtered.map(function( user ){
+                return user;
+            });
+		} else {
+            $scope.listUsers();
+        }
+    };
+    
+    $scope.isNewReservation = function( reservation ) {
+		return reservation!=null && reservation.id==null;
+	};
+    
+    $scope.onUserSelect = function($item, $model, $label) { 
+        $scope.asyncSelected = $label;
+        $scope.reservation.creator = $item.id;
+    };
+    
+    $scope.ok = function () {
+        $modalInstance.close('ok');
+        $scope.saveReservation( $scope.reservation );
+    };                                                                                         
+    
+    $scope.close = function () {
+        $modalInstance.dismiss('cancel');
+    };  
+    
+    $scope.listUsers();
 });
